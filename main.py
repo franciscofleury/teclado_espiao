2d1290ce591c052f363f85a44687dea2b3bd58bf
Francisco Fleury <franmeifleury@gmail.com>
2024-11-09 21:33:35 -0300

    listening keys through mqtt

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import tkinter as tk
-
+import paho.mqtt.client as mqtt
+import ast
 from keyboard_canvas import QWERTYKeyboard
+
+teclas_in = "teclado-espiao/teclas-in"
+teclas_out = "teclado_espiao/teclas-out"
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -29,16 +34,70 @@ class Application(tk.Tk):
         self.main_container.pack(side="right", fill="both", expand=True)
 
         self.frames = {}
+        self.topic_handlers = {}
         self.create_frames()
         self.show_frame("Home")
+        self.start_mqtt()
 
+    def set_topic_handler(self, topic, func):
+        self.topic_handlers[topic] = func
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe(teclas_in)
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        topic = str(msg.topic)
+        message = str(msg.payload.decode("utf-8"))
+        print(topic + " " + message)
+        if topic in self.topic_handlers:
+            self.topic_handlers[topic](msg)
+
+    def start_mqtt(self):
+        print("starting mqtt...")
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+
+        self.mqtt_client.connect("test.mosquitto.org", 1883, 60)
+        self.step_mqtt()
+
+    def step_mqtt(self):
+        self.mqtt_client.loop_read()
+        self.mqtt_client.loop_write()
+        self.mqtt_client.loop_misc()
+        self.after(20, self.step_mqtt)
+
+    def key_listener_factory(self, keyboard, text_box): 
+        def listener(msg):
+            msg = msg.payload.decode('utf-8')
+            print(msg)
+            msg = msg.strip("()")
+            msg = msg.replace(" ", "")
+            action = msg.split(",")
+            print(action)
+            if action[1] == "PRESSED":
+                keyboard.highlight_key(action[0])
+                text_box.insert("end", str(action) + " ")
+            elif action[1] == "RELEASED":
+                keyboard.reset_key(action[0])
+                text_box.insert("end", str(action) + " ")
+            else:
+                print(f"ERROR {action}")
+        return listener
     def create_home_screen(self):
         main_frame = tk.Frame(self.main_container)
         text_box = tk.Text(main_frame, width=100, height=10, bg="white")
         text_box.pack(padx=20, pady=20)
 
         keyboard = QWERTYKeyboard(self, main_frame, main_frame.winfo_width()/2 + 100, main_frame.winfo_height()/2 + 20)
-        keyboard.set_mode("typing", lambda action: print(action))
+        keyboard.set_mode("listen")
+
+        self.set_topic_handler(teclas_in, self.key_listener_factory(keyboard, text_box))
 
         return (main_frame, keyboard)
     
